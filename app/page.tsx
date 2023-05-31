c4d7b4a0e0df8d3f8f8f8cd838a4ae141ec6b2a1
Dhruv Dabhi <dhruvdabhi101@gmail.com>
2023-05-31 19:03:42 +0530

    adding little bit of responsiveness init part two

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ const manrope = Manrope({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-  <div className={"flex justify-center items-center text-white h-screen flex-col gap-4 "+ ` ${manrope.className}`}>
+  <div className={"flex justify-center items-center text-white h-screen flex-col gap-4 max-h-full "+ ` ${manrope.className}`}>
   <div className={'sm:text-6xl text-5xl'}>
      dhruv dabhi
   </div>
