5b71171e2df7da278197413b6fe8c1e0d565802b
Dhruv Dabhi <dhruvdabhi101@gmail.com>
2023-05-31 18:32:55 +0530

    Initial Project

---
+++ Components/Footer/index.tsx
@@ -0,0 +1,10 @@
+export default function Footer(){
+    return(
+        <footer className="footer">
+            <div className="container">
+                <span className="text-muted">Place sticky footer content here.</span>
+            </div>
+        </footer>
+    )
+
+}
