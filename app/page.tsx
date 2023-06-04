ac24ff42146be68eca94cd133df6a71d9e96f45d
Dhruv Dabhi <dhruvdabhi101@gmail.com>
2023-06-04 22:34:21 +0530

    added footer deez nuts

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,23 @@
 import { Manrope } from 'next/font/google'
+import Footer from '@/components/Footer'
 
 const manrope = Manrope({ subsets: ['latin'] })
 
 
 export default function Home() {
   return (
-  <div className={"flex justify-center items-center text-white h-screen flex-col gap-4 max-h-full "+ ` ${manrope.className}`}>
+  <div className={"flex justify-between  text-white h-screen flex-col gap-4 max-h-full p-3 "+ ` ${manrope.className}`}>
+  <div>Profile Picture or something</div>
+  <div className='flex justify-center items-center flex-col'>
   <div className={'sm:text-6xl text-5xl'}>
      dhruv dabhi
   </div>
   <div className='sm:text-2xl text-gray-400 text-xl'>
     blockchain dev
   </div>
+  </div>
+  <Footer/>
+  
   </div>
   )
 }
