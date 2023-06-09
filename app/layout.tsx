2ec65007a43a14e204214460c1245394281ea0c3
Dhruv Dabhi <dhruvdabhi101@gmail.com>
2023-06-09 16:02:13 +0530

    changed some favicon stuff, really tired of these favicons arghhhhh

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,9 +11,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+    <head>
+    <meta charSet="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>
     Dhruv Dabhi
     </title>
+    </head>
       <body>{children} </body>
     </html>
   )
