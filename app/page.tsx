a6ac8fd1e149f6a1330fc034a51984a10b89b5ad
Dhruv Dabhi <dhruvdabhi101@gmail.com>
2023-06-09 16:27:41 +0530

    dont' know what i did

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ const manrope = Manrope({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-  <div className={"flex justify-between  text-white h-screen flex-col gap-4 max-h-full p-4 "+ ` ${manrope.className}`}>
+  <div className={"flex justify-between w-[100%] text-white h-screen flex-col gap-4 max-h-full p-4 "+ ` ${manrope.className}`}>
   <Image src={dimg} alt="img" width={50} height={50}/>
   <div className='flex justify-center items-center flex-col'>
   <div className={'sm:text-6xl text-5xl'}>
