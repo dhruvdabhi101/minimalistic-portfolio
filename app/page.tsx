51efe856e903fe29faa9391254a7a56daf2feb47
Dhruv Dabhi <dhruvdabhi101@gmail.com>
2023-07-28 00:33:25 +0530

    updated website

--- app/page.tsx
+++ app/page.tsx
@@ -3,40 +3,56 @@ import Link from 'next/link'
 import dimg from '../public/DLogog.png'
 import "./globals.css"
 import Image from 'next/image'
+import { VscGithub, VscMail, VscTwitter } from 'react-icons/vsc'
 
 const manrope = Manrope({ subsets: ['latin'] })
 
 
 export default function Home() {
-  return (
-  <div className={"flex justify-between w-[100%] text-white h-screen flex-col gap-4 max-h-full p-4 "+ ` ${manrope.className}`}>
-  <Image src={dimg} alt="img" width={50} height={50}/>
-  <div className='flex justify-center items-center flex-col'>
-  <div className={'sm:text-6xl text-5xl'}>
-     dhruv dabhi
-  </div>
-  <div className='sm:text-2xl text-gray-400 text-xl'>
-    software developer
-  </div>
-  <div className='sm:text-xl text-gray-400 text-xl'>
-  i use vim btw
-  </div>
-  </div>
-        <div className="w-full border-t-2 border-gray-800 px-3 py-1 flex justify-start gap-8 text-lg sm:text-2xl ">
-        <p className="hover:text-gray-400">
-            <Link href="/projects">projects</Link>
-        </p>
-        <p className="hover:text-gray-400">
-            <Link href="https://github.com/dhruvdabhi101">github</Link>
-        </p>
-        <p className="hover:text-gray-400">
-            <Link href="https://linkedin.com/in/dhruvdabhi101">linkedin</Link>
-        </p>
-        <p className="hover:text-gray-400">
-            <Link href="https://dhruvblogs.xyz">blog</Link>
-        </p>
+    return (
+        <div className={"w-full flex flex-row justify-center text-[13px] md:text-[15px] font-thin" + ` ${manrope.className}`}>
+            <div className='flex flex-col justify-center p-5 w-[100%]  sm:w-[80%] md:w-[70%] lg:w-[55%] xl:w-[50%]'>
+                <div className='text-gray-400'>dhruv</div>
+                <div className='flex flex-col gap-6 leading-6'>
+                    <div className='text-gray-400 font-sm'></div>
+                    <div className='text-gray-200'>yo, i'm 20 y/0 <span className='font-medium'>software engineer</span> based in India,going to uni and hating with bottom of my heart. i like blockchain, i love learning different languages</div>
+                    <div className='text-gray-200'>right now, i'm trying to learn solana and rust. i use <span className='font-medium'>vim </span>btw.</div>
+                </div>
+                <div className='text-gray-200 space-y-6 my-10'>
+                    <div className='font-bold text-lg'> Projects </div>
+                    <div className='grid grid-cols-2 md:grid-cols-3 gap-5'>
+                        <div className='flex flex-col gap-3 '>
+                            <a href="https://github.com/dhruvdabhi101/aiject" target='_blank'><div className='underline font-medium '>aiject</div></a>
+                            <div >ai based question maker website to help you with tech projects.</div>
+                        </div>
+                        <div className='flex flex-col gap-3 '>
+                            <a href="https://github.com/dhruvdabhi101/getthatdoc" target='_blank'><div className='underline font-medium '>getthatdoc</div></a>
+                            <div >Notes sharing platform built on PHP</div>
+                        </div>
+                        <div className='flex flex-col gap-3 '>
+                            <a href="https://github.com/dhruvdabhi101/rust-wordl" target='_blank'><div className='underline font-medium '>rust-world</div></a>
+                            <div >command line wordl game built in rust</div>
+                        </div>
+                        <div className='flex flex-col gap-3 '>
+                            <a href="https://github.com/dhruvdabhi101/deez-interpreter" target='_blank'><div className='underline font-medium '>deez interpreter</div></a>
+                            <div >monkey lang interpreter built in rust (half finished)</div>
+                        </div>
+                        <div className='flex flex-col gap-3 '>
+                            <a href="https://github.com/dhruvdabhi101/firunner" target='_blank'><div className='underline font-medium '>firunner</div></a>
+                            <div >neovim plugin to automate python script running built with lua</div>
+                        </div>
+                    </div>
+                </div>
+                <span className='w-full h-[1px]  bg-gray-400'></span>
+                <div className='flex flex-row justify-between my-3 text-gray-400'>
+                    <div>dhruv</div>
+                    <div className='flex flex-row gap-3'>
+                        <a href="https://github.com/dhruvdabhi101" target='_blank'><VscGithub size={20} /></a>
+                        <a href="https://twitter.com/dhruvdabhi101" target='_blank'><VscTwitter size={20}/></a>
+                        <a href="mailto:dhruvdabhi101" target='_blank'><VscMail size={20}/></a>
+                    </div>
+                </div>
+            </div>
         </div>
-  
-  </div>
-  )
+    )
 }
