34665cb642d0c15e7f4f9b3ccd424a2e5e599aeb
Dhruv Dabhi <dhruvdabhi101@gmail.com>
2024-08-03 18:37:23 +0530

    added new package

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import { Analytics } from '@vercel/analytics/react'
 import { GeistSans } from 'geist/font'
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 export default function RootLayout({
   children,
